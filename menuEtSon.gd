a6dbcf3804fbe1b218ae2f93f091bdc32c07194d
Arthur94R <arthurrondeau94@gmail.com>
2025-04-17 10:58:40 +0200

    Ajout du menu introduction

---
+++ menuEtSon.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	$FmodEventEmitter2D.set_parameter("IntroLoop", "Intro")
+	$FmodEventEmitter2D.play()
+	
+	$Control/MarginContainer/VBoxContainer/LancerJeu.pressed.connect(_on_LancerJeu_pressed)
+	$Control/MarginContainer/VBoxContainer/QuitterJeu.pressed.connect(_on_QuitterJeu_pressed)
+
+# Fonction appelée quand le bouton LancerJeu est pressé
+func _on_LancerJeu_pressed() -> void:
+	# Charger et changer pour la scène du jeu
+	get_tree().change_scene_to_file("res://LastTrain.tscn")
+
+# Fonction appelée quand le bouton QuitterJeu est pressé
+func _on_QuitterJeu_pressed() -> void:
+	# Quitter le jeu
+	get_tree().quit()
